adf2752a2030216759dd411637f7c4492d049d52
yamato090 <tomoki.y.0924@icloud.com>
2021-11-02 10:03:15 +0000

    add Rspacクリア

--- bookers2-debug/app/controllers/books_controller.rb
+++ bookers2-debug/app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+ before_action :colect_user, only: [:edit, :update]
 
   def show
     @book = Book.find(params[:id])
@@ -47,7 +48,12 @@ class BooksController < ApplicationController
 
 
   private
-
+  def colect_user
+    user = Book.find(params[:id]).user
+    if current_user.id != user.id
+      redirect_to books_path
+    end
+  end
   def book_params
     params.require(:book).permit(:title, :body)
   end
--- bookers2-debug/app/controllers/users_controller.rb
+++ bookers2-debug/app/controllers/users_controller.rb
@@ -34,14 +34,15 @@ class UsersController < ApplicationController
   end
 
   private
-  def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image)
-  end
-
   def ensure_correct_user
     @user = User.find(params[:id])
     unless @user == current_user
       redirect_to user_path(current_user)
     end
   end
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+
 end
